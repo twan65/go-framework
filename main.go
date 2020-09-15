046d9b5c3d7af35d329886be3a01886ed50c9cba
twan65 <cx.twan65@gmail.com>
2020-09-15 22:37:35 +0900

    main修正

--- main.go
+++ main.go
@@ -3,17 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"route"
 )
 
 func main() {
+	r := &route.Router{Handlers: make(map[string]map[string]http.HandlerFunc)}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "wellcom!")
+	r.HandleFunc("GET", "/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "welcome!")
 	})
 
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("GET", "/about", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "about")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	r.HandleFunc("GET", "/users/:id", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "retrieve user")
+	})
+
+	r.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "retrieve user’s address")
+	})
+
+	r.HandleFunc("POST", "/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "create user")
+	})
+
+	r.HandleFunc("POST", "/users/:user_id/addresses", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "create user’s address")
+	})
+
+	http.ListenAndServe(":8080", r)
 }
